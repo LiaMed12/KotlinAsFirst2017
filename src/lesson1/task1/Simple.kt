cb112d003200f2dac26a5b1c09791d802262f44d
LiaMed12 <lia.medvevdeva2017@yandex.ru>
2017-09-20 00:41:25 +0300

    Я реализовала все функции

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -101,7 +101,7 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double =initial*(1+ pow(percent*0.01,3.0))
+fun accountInThreeYears(initial: Int, percent: Int): Double =initial*(pow((1+ percent*0.01),3.0))
 
 /**
  * Простая
