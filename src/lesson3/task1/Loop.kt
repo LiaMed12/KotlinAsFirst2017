ba917895faa8e7992501ac3333c5a403c85802e2
LiaMed12 <lia.medvevdeva2017@yandex.ru>
2017-10-02 21:58:19 +0300

    Исправлена функция

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -138,7 +138,15 @@ fun minDivisor(n: Int): Int {
  *
  * Для заданного числа n > 1 найти максимальный делитель, меньший n
  */
-fun maxDivisor(n: Int): Int = TODO()
+fun maxDivisor(n: Int): Int {
+    var MaxK = 1
+    for ( i in 1..n) {
+        if (n%i==0 && n!=i){
+            MaxK = i
+        }
+    }
+    return MaxK
+}
 
 /**
  * Простая
@@ -147,8 +155,14 @@ fun maxDivisor(n: Int): Int = TODO()
  * Взаимно простые числа не имеют общих делителей, кроме 1.
  * Например, 25 и 49 взаимно простые, а 6 и 8 -- нет.
  */
-fun isCoPrime(m: Int, n: Int): Boolean = TODO()
-
+fun isCoPrime(m: Int, n: Int): Boolean {
+    var CommonFactor = 0
+    for ( i in 2..n){
+          if (m%i==0 && n%i==0 && m!=n)
+              CommonFactor+=i
+    }
+    return CommonFactor == 0
+}
 /**
  * Простая
  *
